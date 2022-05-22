8d84d9c389d05133f5a3db5c57950073b4071f3c
naduni123 <79349659+naduni123@users.noreply.github.com>
2022-05-22 17:12:10 +0530

    create student group

--- backend/controllers/student-groups/studentGroups.controller.js
+++ backend/controllers/student-groups/studentGroups.controller.js
@@ -8,13 +8,29 @@ const getStdGroups =async (req, res)=>{
 }
 
 const createStdGroups = async (req, res)=>{
-    if(!req.body.name){
+    if(!req.body.member1Id){
         res.status(400)
-        throw new Error('please add the name')
+        throw new Error('please Enter the Member 01 IT number')
+    }
+    if(!req.body.member2Id){
+        res.status(400)
+        throw new Error('please Enter the member 02 IT number')
+    }
+    if(!req.body.member3Id){
+        res.status(400)
+        throw new Error('please Enter the member 03 IT number')
+    }
+    if(!req.body.member4Id){
+        res.status(400)
+        throw new Error('please Enter the member 04 IT number')
     }
 
     const group =StudentGroup.create({
-        name:req.body.name
+        topic:req.body.topic,
+        member1Id:req.body.member1Id,
+        member2Id:req.body.member2Id,
+        member3Id:req.body.member3Id,
+        member4Id:req.body.member4Id,
     })
 res.status(200).json(group)
 }
--- backend/models/student-groups/studentGroup.model.js
+++ backend/models/student-groups/studentGroup.model.js
@@ -2,10 +2,41 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const studentGroupSchema = new Schema({
-  name: {
+  topic: {
     type: String,
     required: true,
-  }
+  },
+  member1Id: {
+    type: String,
+    required: true,
+  },
+  member2Id: {
+    type: String,
+    required: true,
+  },
+  member3Id: {
+    type: String,
+    required: true,
+  },
+  member4Id: {
+    type: String,
+    required: true,
+  },
+  supervisorId: {
+    type: String,
+  },
+  cosuperviserId: {
+    type: String,
+  },
+  panelMembersId: {
+    type: [],
+  },
+  supervisorStatus: {
+    type: Boolean,
+  },
+  cosupervisorStatus: {
+    type: Boolean,
+  },
 });
 
 module.exports = mongoose.model("StudentGroup", studentGroupSchema);
